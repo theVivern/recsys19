6bba596df1555e1417bab7a3016944cbcccdb746
Jakob Pinterits <jakob.pinterits@gmail.com>
2019-05-26 01:49:09 +0200

    added session preprocessing

--- src/common.py
+++ src/common.py
@@ -2,6 +2,12 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn.preprocessing import MultiLabelBinarizer, FunctionTransformer, LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline, TransformerMixin
+from sklearn.base import BaseEstimator
+from sklearn_pandas import DataFrameMapper
+import gc
 
 
 # %% Important project paths
@@ -14,48 +20,7 @@ cache_dir = root_dir / 'cache'
 cache_dir.mkdir(parents=True, exist_ok=True)
 
 
-# %% Dataset loading
-
-def load_sessions(path: Path, nrows: int = None):
-    df = pd.read_csv( str(path),
-        header=0,
-        nrows=nrows,
-        dtype={
-        })
-
-    # raise NotImplementedError
-
-    return df
-
-
-def load_metadata(path: Path, nrows: int = None):
-    df = pd.read_csv( str(path),
-        header=0,
-        nrows = nrows,
-        dtype={
-        })
-
-    return df
-
-
-def load_dataset(nrows: int = None):
-    df_train = load_sessions(data_dir / 'train.csv', nrows)
-    df_test = load_sessions(data_dir / 'test.csv', nrows)
-    df_meta = load_metadata(data_dir / 'metadata.csv')
-
-    return df_train, df_test, df_meta
-
-# df_train = load_sessions(data_dir / 'train.csv', 1000)
-df_meta = load_metadata(data_dir / 'item_metadata.csv', 1000)
-
-
-# %%
-from sklearn.preprocessing import MultiLabelBinarizer, FunctionTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline, TransformerMixin
-from sklearn.base import BaseEstimator
-
-
+# %% Load the metadata
 class MetadataEncoder(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.encoder = MultiLabelBinarizer()
@@ -81,11 +46,102 @@ class MetadataEncoder(BaseEstimator, TransformerMixin):
         )
 
 
-meta_pipe = MetadataEncoder()
-meta_pipe.fit(df_meta)
-tmp = meta_pipe.transform(df_meta)
+def load_metadata(path: Path = None, nrows: int = None):
+    if path is None:
+        path = data_dir / 'item_metadata.csv'
+
+    # Read
+    df = pd.read_csv( str(path),
+        header=0,
+        nrows = nrows,
+        dtype={
+            'item_id': np.int64
+        })
+
+    # Preprocess
+    pipe = MetadataEncoder()
+    df = pipe.fit_transform(df)
+
+    return df
+
+data_meta = load_metadata()
+print(f'Loaded {len(data_meta)} rows of metadata')
 
 
+# %% Load the sessions
+
+# Free up memory
+try:
+    del data_sessions
+except NameError:
+    pass
+else:
+    gc.collect()
 
 
-#%%
+def load_session_data(path: Path, nrows: int = None):
+    df = pd.read_csv( str(path),
+        header=0,
+        nrows=nrows,
+        dtype={
+            'timestamp': np.int64
+        })
+
+    return df
+
+
+# LabelEncoders for label columns
+label_columns = [
+    'user_id', 'session_id',
+    'action_type', 'platform', 'city', 'device'
+]
+
+label_columns = {
+    k: LabelEncoder() for k in label_columns
+}
+
+
+def load_sessions(nrows: int = None):
+    # Load the data
+    data_train = load_session_data(data_dir / 'train.csv', nrows=nrows)
+    data_test = load_session_data(data_dir / 'test.csv', nrows=nrows)
+
+    # Merge the dataframes
+    data_train['is_train'] = True
+    data_test['is_train'] = False
+
+    data = pd.concat(
+        (data_test, data_train),
+        axis=0
+    )
+
+    del data_train, data_test
+    gc.collect()
+
+    # Preprocess the columns
+    # ... timestamp
+    data.timestamp = pd.to_datetime(
+        data.timestamp,
+        unit='s',
+        utc=True
+    )
+
+    # ... convert categorical strings to integers
+    for colname, encoder in label_columns.items():
+        col = encoder.fit_transform(data[colname])
+
+        # Save memory
+        assert len(encoder.classes_) < 10e6, (colname, len(encoder.classes_))
+        col = col.astype(np.int32)
+
+        data[colname] = col
+
+    return data
+
+
+data_sessions = load_sessions()
+print(f'Loaded {len(data_sessions)} rows of session data')
+n_bytes = data_sessions.memory_usage().sum()
+print(f'Session data is using {n_bytes/1e6:.2f}MB')
+
+
